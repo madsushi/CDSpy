1806d0515eef3f9fa0f77198edde4f1032182034
Chase Christian <madsushi@gmail.com>
2015-03-28 23:34:08 -0700

    basic addon toggling UI

---
+++ CDSpyConfig.lua
@@ -0,0 +1,52 @@
+local addonName, addon = ...
+if not addon.healthCheck then return end
+
+local frame = addon.frame
+frame.name = addonName
+frame:Hide()
+
+frame:SetScript("OnShow", function(frame)
+
+	local function newCheckbox(label, description, onClick)
+		local check = CreateFrame("CheckButton", "CDSpy" .. label, frame, "InterfaceOptionsCheckButtonTemplate")
+		check:SetScript("OnClick", function(self)
+			PlaySound(self:GetChecked() and "igMainMenuOptionCheckBoxOn" or "igMainMenuOptionCheckBoxOff")
+			onClick(self, self:GetChecked() and true or false)
+		end)
+		check.label = _G[check:GetName() .. "Text"]
+		check.label:SetText(label)
+		check.tooltipText = label
+		check.tooltipRequirement = description
+		return check
+	end
+
+	local title = frame:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge")
+	title:SetPoint("TOPLEFT", 16, -16)
+	title:SetText(addonName)
+
+	local override_box = newCheckbox(
+		"Global Enable",
+		"Really?",
+		function(self, value) CDSpyDB.override = value end)
+	override_box:SetChecked(CDSpyDB.override)
+	override_box:SetPoint("TOPLEFT", title, "BOTTOMLEFT", -2, -16)
+
+	local taunt_toggle_box = newCheckbox(
+		"Enable Taunt announcements",
+		"OK whatever",
+		function(self, value) CDSpyDB.taunt_toggle = value end)
+	taunt_toggle_box:SetChecked(CDSpyDB.taunt_toggle)
+	taunt_toggle_box:SetPoint("TOPLEFT", override_box, "BOTTOMLEFT", 0, -8)
+
+	local pug_toggle_box = newCheckbox(
+		"Enable LFR",
+    "dude",
+		function(self, value) CDSpyDB.pug_toggle = value end)
+  pug_toggle_box:SetChecked(CDSpyDB.pug_toggle)
+	pug_toggle_box:SetPoint("TOPLEFT", taunt_toggle_box, "BOTTOMLEFT", 0, -8)
+
+
+	frame:SetScript("OnShow", nil)
+end)
+InterfaceOptions_AddCategory(frame)
+
